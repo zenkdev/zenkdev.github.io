d57c6e4231c5b0b4e62a85e47b513933c6e969b6
Сенько Александр <aleksandr.senko@gazprombank.ru>
2025-10-13 20:51:04 +0300

    Add more skills

--- src/app.config.ts
+++ src/app.config.ts
@@ -3,6 +3,23 @@ const config = {
   contactVisible: false,
   linkedin: "https://www.linkedin.com/in/alexander-senko-9a7b201a0/",
   github: "https://zenkdev.github.io/",
+  skills: [
+    { name: "React", image: "/react-svgrepo-com.svg" },
+    { name: "JavaScript", image: "/logo-js-svgrepo-com.svg" },
+    { name: "TypeScript", image: "/logo-ts-svgrepo-com.svg" },
+    { name: "Node.js", image: "/node-js-svgrepo-com.svg" },
+    { name: "Redux", image: "/redux-svgrepo-com.svg" },
+    { name: "Mobx", image: "/mobx-svgrepo-com.svg" },
+    { name: "Zustand", image: "/zustand-original.svg" },
+    { name: "HTML5", image: "/html5-svgrepo-com.svg" },
+    { name: "CSS3", image: "/css3-svgrepo-com.svg" },
+    { name: "Tailwind CSS", image: "tailwindcss-svgrepo-com.svg" },
+    { name: "Styled Components", image: "/styled-components-svgrepo-com.svg" },
+    { name: "Jest", image: "/jest-svgrepo-com.svg" },
+    { name: "Feature Sliced Design", image: "/fsd-logo.svg" },
+    { name: "Git", image: "/git-svgrepo-com.svg" },
+    { name: "Figma", image: "/figma-svgrepo-com.svg" },
+  ],
 };
 
 export default config;
